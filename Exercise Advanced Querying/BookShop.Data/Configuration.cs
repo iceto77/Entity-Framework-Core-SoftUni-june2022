eff58489a2e1fa57948e66740ff4536aa221ad1e
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2022-07-22 15:49:43 +0300

    Update Configuration.cs

--- Exercise Advanced Querying/BookShop.Data/Configuration.cs
+++ Exercise Advanced Querying/BookShop.Data/Configuration.cs
@@ -3,6 +3,6 @@
     internal class Configuration
     {
         internal static string ConnectionString 
-            => @"Server=localhost;Database=BookShop;User Id=sa;Password=Ic0135796842;";
+            => @"Server=localhost;Database=BookShop;User Id=sa;Password=xxxxxx;";
     }
 }
