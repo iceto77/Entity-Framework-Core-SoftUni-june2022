011b69f93d94dc080731fcacf4379071e7a56758
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2022-07-22 15:50:24 +0300

    Update Config.cs

--- Exercise ADO.NET/Problem03. Minion Names/Config.cs
+++ Exercise ADO.NET/Problem03. Minion Names/Config.cs
@@ -2,6 +2,6 @@
 {
     public static class Config
     {
-        public const string ConnectionString = @"Server=localhost;Database=MinionsDB;User Id=sa;Password=Ic0135796842;";
+        public const string ConnectionString = @"Server=localhost;Database=MinionsDB;User Id=sa;Password=xxxxxx;";
     }
 }
