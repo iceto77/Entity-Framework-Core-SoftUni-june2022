a62ce637a61d6cb082861bd00da030f1be4455e0
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2022-07-22 15:46:22 +0300

    Update Configuration.cs

--- Exercise XML Processing/Car Dealer/Data/Configuration.cs
+++ Exercise XML Processing/Car Dealer/Data/Configuration.cs
@@ -3,6 +3,6 @@ namespace ProductShop.Data
 {
     public static class Configuration
     {
-        public const string ConnectionString = @"Server=localhost;Database=CarDealerXML;User Id=sa;Password=Ic0135796842;";
+        public const string ConnectionString = @"Server=localhost;Database=CarDealerXML;User Id=sa;Password=xxxxx;";
     }
 }
