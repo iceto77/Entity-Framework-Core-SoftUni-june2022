c9e5d7fed8c0ddfc4d1125ef4dc9fcbd90707ed7
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2022-08-05 14:44:06 +0300

    Delete Exam Preparation/C# DB Advanced Exam - 08 August 2020 directory

--- Exam Preparation/C# DB Advanced Exam - 08 August 2020/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VaporStore.Data
-{
-	public static class Configuration
-	{
-		public static string ConnectionString =
-			@"Server=.;Database=VaporStore;User Id=sa;Password=Ic0135796842;";
-	}
-}
